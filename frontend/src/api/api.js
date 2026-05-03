15df49c03caddedf92eb2b1ccf5b11c9bb36449f
Ademoye Haruna Adegoke <Hardecomm>
2026-05-03 18:53:29 +0100

    update package

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,12 +1,11 @@
-// src/api/analyzeApi.js
 import axios from "axios";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
-export const analyzePlateImage = async (imageFile) => {
+export const analyzePlate = async (imageFile) => {
   const formData = new FormData();
-  formData.append("image", imageFile); // ✅ must match backend upload.single("image")
+  formData.append("image", imageFile);
 
   const response = await axios.post(`${API_BASE_URL}/api/analyze`, formData, {
     headers: {
